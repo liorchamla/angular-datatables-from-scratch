46404a0d8c4c0bdcf594df1c9935bb926a6ed038
Lior Chamla <lchamla@gmail.com>
2018-01-14 01:59:06 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  // Our array of clients
+  clients: any[];
+  dataTable: any;
+
+  constructor(private http: HttpClient, private chRef: ChangeDetectorRef){}
+
+  ngOnInit(){
+    this.http.get('https://5a5a9e00bc6e340012a03796.mockapi.io/clients')
+      .subscribe((data: any[]) => {
+        this.clients = data;
+
+        // You'll have to wait that changeDetection occurs and projects data into 
+        // the HTML template, you can ask Angular to that for you ;-)
+        this.chRef.detectChanges();
+
+        // Now you can use jQuery DataTables :
+        const table: any = $('table');
+        this.dataTable = table.DataTable();
+      });
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
